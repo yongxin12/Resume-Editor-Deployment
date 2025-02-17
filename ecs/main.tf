d3e5e29c48ae215bf860233e0dd3cad752fce36a
Rex <yongxin1@ualberta.ca>
2025-02-16 23:41:34 -0700

    tiny changes

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,5 +1,12 @@
 # Main entry point
 
+
+# Provider configuration
+provider "aws" {
+  region = "us-east-2"
+}
+
+
 terraform {
   backend "s3" {
     bucket = "resume-editor-terraform-state-bucket"
@@ -12,8 +19,9 @@ terraform {
 
 # Create a VPC
 resource "aws_vpc" "production_vpc" {
-  cidr_block = var.vpc_cidr_block
-
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name = "production-vpc"
   }
@@ -546,6 +554,7 @@ resource "aws_ecs_task_definition" "editor_backend" {
       name      = "editor_backend"
       image     = "376129840507.dkr.ecr.us-east-2.amazonaws.com/editor-backend:latest"
       cpu       = 256
+      memory    = 512
       essential = true
 
       portMappings = [
@@ -683,11 +692,13 @@ resource "aws_ecs_service" "editor_backend_service" {
   task_definition = aws_ecs_task_definition.editor_backend.arn
   desired_count   = 2
   launch_type     = "FARGATE"
+
   network_configuration {
     subnets          = aws_subnet.public[*].id
     security_groups  = [aws_security_group.allow_web.id]
     assign_public_ip = true
   }
+
   load_balancer {
     target_group_arn = aws_lb_target_group.editor_backend_tg.arn
     container_name   = "editor_backend"
--- flow.md
+++ flow.md
@@ -9,10 +9,34 @@
 9) ssh -i id_rsa ec2-user@<your_ec2_ip_address>
 10) touch build.sh | vim build.sh | chmod +x build.sh | ./build.sh
 
+docker run -d --name market-frontend -p 3000:80 376129840507.dkr.ecr.us-east-2.amazonaws.com/market-frontend:latest
 
-
-aws ecr create-repository --repository-name resume_modifier_market_frontend --region us-east-2
+aws ecr create-repository --repository-name editor-frontend --region us-east-2
 
 
 
 aws logs create-log-group --log-group-name "/ecs/market_backend" --region us-east-2
+
+
+aws ecs execute-command \
+  --cluster resume-editor \
+  --task 99cc508bd4304181922816cb6f99e60a \
+  --container market-frontend \
+  --interactive \
+  --command "/bin/bash"
+
+
+aws ecs update-service \
+  --cluster resume-editor \
+  --service market-frontend \
+  --enable-execute-command
+
+
+
+  aws ecs update-service \
+  --cluster resume-editor \
+  --service market-frontend \
+  --task-definition market_frontend_family:32 \
+  --enable-execute-command
+
+
