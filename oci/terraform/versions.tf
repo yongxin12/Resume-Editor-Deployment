76b1ed8cb4ae47d07963a409b64a688da4dd4bb9
Rex <yongxin1@ualberta.ca>
2025-04-24 23:37:12 -0600

    transfer terraform.tfstate to object storage instead of local

--- oci/terraform/versions.tf
+++ oci/terraform/versions.tf
@@ -7,3 +7,29 @@ terraform {
   }
   required_version = ">= 1.0.0"
 }
+
+# # (Terraform version >= 1.6.4)
+# terraform {
+#   backend "s3" {
+#     bucket                    = "bucket-20250424-2214"  #"terraform-states"
+#     region                    = "ca-toronto-1"
+#     key                       = "tf.tfstate"
+#     skip_region_validation      = true
+#     skip_credentials_validation = true
+#     skip_requesting_account_id  = true
+#     use_path_style              = true
+#     skip_s3_checksum            = true
+#     skip_metadata_api_check = true
+#     endpoints = {
+#       # s3 = "https://<namespace>.compat.objectstorage.<region>.oraclecloud.com"
+#       s3 = "https://objectstorage.ca-toronto-1.oraclecloud.com/p/Qzv9fgQfASxJ0Xw9-gcL0U9kJEMtOCp0Hzbll6XkpMAKJ2dKo7NyjK7U3-hASQuK/n/yztdqx0npeqw/b/bucket-20250424-2214/o/"
+#     }
+#   }
+# }
+
+terraform {
+  backend "http" {
+    update_method = "PUT"
+    address       = "https://objectstorage.ca-toronto-1.oraclecloud.com/p/Qzv9fgQfASxJ0Xw9-gcL0U9kJEMtOCp0Hzbll6XkpMAKJ2dKo7NyjK7U3-hASQuK/n/yztdqx0npeqw/b/bucket-20250424-2214/o/terraform.tfstate"
+  }
+}
